c66294ed56592dc677200d00d8854c3153d9a53b
Eric <ea@sos-paris.com>
2015-10-29 18:19:04 +0100

    Requetes accessibles a partir du ruban

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace Arii\ATSBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Yaml\Parser;
 
 class DefaultController extends Controller
 {
@@ -31,6 +32,24 @@ class DefaultController extends Controller
 
     public function ribbonAction()
     {
+        // On recupère les requetes
+        $yaml = new Parser();
+        $lang = $this->getRequest()->getLocale();
+        $basedir = '../src/Arii/ATSBundle/Resources/views/Requests/'.$lang;
+        $Requests = array();
+        if ($dh = @opendir($basedir)) {
+            while (($file = readdir($dh)) !== false) {
+                if (substr($file,-4) == '.yml') {
+                    $content = file_get_contents("$basedir/$file");
+                    $v = $yaml->parse($content);
+                    $v['id']=substr($file,0,strlen($file)-4);
+                    if (!isset($v['icon'])) $v['icon']='cross';
+                    if (!isset($v['title'])) $v['title']='?';
+                    array_push($Requests, $v);
+                }
+            }
+        }
+        
         // On recupere la liste des base de données
         // si il y en a plus d'une, pour ats, on cree une liste de choix
         $session = $this->container->get('arii_core.session');
@@ -46,7 +65,7 @@ class DefaultController extends Controller
         $response = new Response();
         $response->headers->set('Content-Type', 'application/json');
         
-        return $this->render('AriiATSBundle:Default:ribbon.json.twig',array('Databases' => $Databases), $response );
+        return $this->render('AriiATSBundle:Default:ribbon.json.twig',array('Databases' => $Databases, 'Requests' => $Requests ), $response );
     }
 
     public function docAction() {
